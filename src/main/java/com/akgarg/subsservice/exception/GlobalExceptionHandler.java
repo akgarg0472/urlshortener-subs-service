bc48ba4af32b4d665d1ffa7523e01543cbcb5a16
akgarg0472 <akgarg0472@gmail.com>
2025-01-15 23:41:35 +0530

    subscription pack bug fixes

--- src/main/java/com/akgarg/subsservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/akgarg/subsservice/exception/GlobalExceptionHandler.java
@@ -34,7 +34,6 @@ public class GlobalExceptionHandler {
         if (log.isDebugEnabled()) {
             log.error("Exception", e);
         }
-        e.printStackTrace();
         final ApiErrorResponse errorResponse = switch (e) {
             case HttpRequestMethodNotSupportedException ex ->
                     methodNotAllowedErrorResponse("Request HTTP method '" + ex.getMethod() + "' is not allowed. Allowed: " + Arrays.toString(ex.getSupportedMethods()));
